1b9819a10bbf7e6f856ba249087f28902da170f3
Amit Kumar <amitkumar03022004@gmail.com>
2024-01-08 03:21:45 +0530

    project setup update frontend

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+
+//to set view engine ejs
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+app.use(express.static(path.join(__dirname,"public")));
+//set path for post method
+app.use(express.urlencoded({extended:true}));
+
+
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+})
+
+app.post("/submit",(req,res)=>{
+    let {thala} = req.body;
+    console.log(thala);
+    let result = thalaFunction(thala);
+    if(result === "success"){
+        res.render("success.ejs");
+    }else{
+       res.render("failure.ejs");
+    }
+    
+
+})
+app.listen(port,()=>{
+    console.log(`listening to port ${port}`);
+})
+
+//function section
+function thalaFunction(value){
+    if(value == 7){
+        return "success";
+    }
+    if(value.length == 7){
+        return "success";
+    }else{
+        return "failure";
+    }
+}
